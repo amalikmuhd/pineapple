3b6d84dca0778aace460874011b5ea3a61d39b97
Abdulmalik Muhammad <bawasuru@gmail.com>
2022-03-04 03:44:32 +0100

    Project Structure

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:pineapple/imports/imports.dart';
+
+const kPrimaryColor = Color(0xFF6f35a5);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+//Images
+const kMainBottom = "assets/images/main_bottom.png";
+const kMainTop = "assets/images/main_top.png";
